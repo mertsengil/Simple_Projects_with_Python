c9b66d1033a2141411006598c4e25f0717711640
Mert Şengil <89454774+mertsengil@users.noreply.github.com>
2021-10-03 00:03:10 +0300

    Add files via upload
    Girdiğiniz iki sayının ebob'unu bulan program

---
+++ Ebob(x,y).py
@@ -0,0 +1,15 @@
+def ebob_bulma(sayı1, sayı2):
+    i = 1
+    ebob = 1
+    while (i <= sayı1 and i <= sayı2):
+
+        if (not (sayı1 % i) and not (sayı2 % i)):
+            ebob = i
+        i += 1
+    return ebob
+
+
+sayı1 = int(input("Sayı-1:"))
+sayı2 = int(input("Sayı-2:"))
+
+print("Ebob:", ebob_bulma(sayı1, sayı2))
