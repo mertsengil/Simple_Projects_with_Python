b233677bd97ecee63fb8e6ab8511fd3b7ba88c47
Mert Şengil <89454774+mertsengil@users.noreply.github.com>
2021-10-07 16:07:41 +0300

    football_player.py

--- football project/futbolcular.py
+++ football project/futbolcular.py
@@ -1,1 +1,25 @@
+with open("Futbolcular.txt","r") as file:
+    fenerbahce11 = list()
+    galatasaray11 = list()
+    besiktas11 = list()
+    for satir in file:
+        satir = satir[:-1]
+        liste = satir.split(",")
+        takim = liste[1]
+        isim = liste[0]
+        if (takim == "Fenerbahçe"):
+            fenerbahce11.append(isim + "\n")
+        elif (takim == "Galatasaray"):
+            galatasaray11.append(isim + "\n")
+        else:
+            besiktas11.append(isim + "\n")
 
+    with open("Galatasaray.txt","w",encoding= "utf-8") as file1:
+        for i in galatasaray11:
+            file1.write(i)
+    with open("Fenerbahce.txt","w",encoding= "utf-8") as file2:
+        for i in fenerbahce11:
+            file2.write(i)
+    with open("Besiktas.txt","w",encoding= "utf-8") as file3:
+        for i in besiktas11:
+            file3.write(i)
